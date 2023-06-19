4136b987028237f86895fda038bab59172070958
arnavbahadur <bahadurarnav2203@gmail.com>
2023-06-19 21:12:24 +0530

    publish

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,15 +7,15 @@ export default function Footer() {
       <div className="row-cols-5 justify-content-md-center">
 
 
-        <a className='col col-lg-2 mx-2' href='https://github.com/sharma39vishal/'>        
+        <a className='col col-lg-2 mx-2' href='https://github.com/arnavbahadur'>        
           <img src='https://img.icons8.com/clouds/344/github.png' width={"10%"} height={"13%"}/>
          </a>
         
-        <a className='col-md-auto mx-2' href='https://www.linkedin.com/in/sharma39vishal/'>
+        <a className='col-md-auto mx-2' href='https://www.linkedin.com/in/arnav-bahadur-5051201ba/'>
         <img src='https://img.icons8.com/nolan/344/linkedin.png' width={"10%"} height={"13%"}/>
        </a>
        
-       <a className='col col-lg-2 mx-2' href='mailto:sharma39vishal@gmail.com'>
+       <a className='col col-lg-2 mx-2' href='mailto:bahadurarnav2203@gmail.com'>
       <img src='https://img.icons8.com/clouds/344/gmail-new.png' width={"10%"} height={"13%"}/>
       </a>
 
@@ -23,7 +23,7 @@ export default function Footer() {
 </div>
 
 </div>
-<p className="text-center">Copyright @2022 | Designed by <a href="https://www.linkedin.com/in/sharma39vishal/">Vishal Sharma</a></p>
+<p className="text-center">Copyright @2023 | Designed by <a href="https://www.linkedin.com/in/arnav-bahadur-5051201ba/">Arnav Bahadur</a></p>
 
 </div>
     )
